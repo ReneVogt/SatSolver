443ac559f89e589ca22443e10198142de3a9126d
René Vogt <rene.vogt@gmx.de>
2024-11-28 16:01:38 +0100

    set up unit tests structure for parser tests

--- src/SatSolver/SatSolver/Clause.cs
+++ src/SatSolver/SatSolver/Clause.cs
@@ -1,4 +1,8 @@
 ﻿namespace Revo.SatSolver;
 
-public sealed record Clause(Literal[] Literals);
+public sealed record Clause(Literal[] Literals)
+{
+    public static implicit operator Clause(Literal[] literals) => new(literals);
+    public static implicit operator Clause(int[] literals) => new(literals.Select(i => (Literal)i).ToArray());
+}
 
