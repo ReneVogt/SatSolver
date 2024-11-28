42cef74d1d879e4e97a278d4152fe6ba0664496b
René Vogt <rene.vogt@gmx.de>
2024-11-28 14:15:19 +0100

    refactor parser

--- src/SatSolver/SatSolver/Dimacs/CnfParserException.cs
+++ src/SatSolver/SatSolver/Dimacs/CnfParserException.cs
@@ -8,42 +8,45 @@ public sealed class CnfParserException : Exception
     public enum Reason
     {
         Unknown = 0,
+        InvalidLine,
+        InvalidCharacter,
         MissingProblemLine,
         InvalidProblemFormat,
         LiteralOutOfRange,
         MissingClauses,
         MissingLiteral,
-        MissingTerminator,
-        InvalidCharacter
+        MissingTerminator
     }
 
     public int Line { get; }
-    public int Part { get; }
+    public int Column { get; }
     public Reason Error { get; }
 
-    CnfParserException(string message, int line, int part, Reason error) : base(message) => (Line, Part, Error) = (line, part, error);
+    CnfParserException(string message, int line, int column, Reason error) : base(message) => (Line, Column, Error) = (line, column, error);
 
-    static readonly CompositeFormat _missingProblemLine = CompositeFormat.Parse(Resources.CnfParserException_MissingProblemLine);
-    internal static CnfParserException MissingProblemLine(int line) => new(string.Format(null, _missingProblemLine, line), line, 0, Reason.MissingProblemLine);
+    static readonly CompositeFormat _invalidLine = CompositeFormat.Parse(Resources.CnfParserException_InvalidLine);
+    internal static CnfParserException InvalidLine(int line) => new(string.Format(null, _invalidLine, line), line, 0, Reason.InvalidLine);
+
+    static readonly CompositeFormat _invalidCharacter = CompositeFormat.Parse(Resources.CnfParserException_InvalidCharacter);
+    internal static CnfParserException InvalidCharacter(string text, int line, int column) => new(string.Format(null, _invalidCharacter, text, line, column), line, column, Reason.InvalidCharacter);
+
+
+    static readonly CompositeFormat _invalidProblemLine = CompositeFormat.Parse(Resources.CnfParserException_InvalidProblemLine);
+    internal static CnfParserException InvalidProblemLine(int line, int column = 0) => new(string.Format(null, _invalidProblemLine, line), line, column, Reason.MissingProblemLine);
 
     static readonly CompositeFormat _invalidProblemFormat = CompositeFormat.Parse(Resources.CnfParserException_InvalidProblemFormat);
     internal static CnfParserException InvalidProblemFormat(string format, int line) => new(string.Format(null, _invalidProblemFormat, format, line), line, 1, Reason.InvalidProblemFormat);
 
 
     static readonly CompositeFormat _literalOutOfRange = CompositeFormat.Parse(Resources.CnfParserException_LiteralOutOfRange);
-    internal static CnfParserException LiteralOutOfRange(int literal, int max, int line, int part) => new(string.Format(null, _literalOutOfRange, literal, max, line, part), line, part, Reason.LiteralOutOfRange);
+    internal static CnfParserException LiteralOutOfRange(int literal, int max, int line, int column) => new(string.Format(null, _literalOutOfRange, literal, max, line, column), line, column, Reason.LiteralOutOfRange);
 
     static readonly CompositeFormat _missingliteral = CompositeFormat.Parse(Resources.CnfParserException_MissingLiteral);
-    internal static CnfParserException MissingLiteral(int line, int part) => new(string.Format(null, _missingliteral, line, part), line, part, Reason.MissingLiteral);
+    internal static CnfParserException MissingLiteral(int line, int column) => new(string.Format(null, _missingliteral, line, column), line, column, Reason.MissingLiteral);
 
     static readonly CompositeFormat _missingTerminator = CompositeFormat.Parse(Resources.CnfParserException_MissingTerminator);
-    internal static CnfParserException MissingTerminator(int line, int part) => new(string.Format(null, _missingTerminator, line, part), line, part, Reason.MissingTerminator);
+    internal static CnfParserException MissingTerminator(int line, int column) => new(string.Format(null, _missingTerminator, line, column), line, column, Reason.MissingTerminator);
 
     static readonly CompositeFormat _missingClauses = CompositeFormat.Parse(Resources.CnfParserException_MissingClauses);
-    internal static CnfParserException MissingClauses(int clauses, int line) => new(string.Format(null, _missingClauses, clauses), line, 0, Reason.MissingClauses);
-
-
-
-    static readonly CompositeFormat _invalidCharacter = CompositeFormat.Parse(Resources.CnfParserException_InvalidCharacter);
-    internal static CnfParserException InvalidCharacter(string text, int line, int part) => new(string.Format(null, _invalidCharacter, text, line, part), line, part, Reason.InvalidCharacter);
+    internal static CnfParserException MissingClauses(int missingClauses, int line) => new(string.Format(null, _missingClauses, missingClauses, line), line, 0, Reason.MissingClauses);
 }
