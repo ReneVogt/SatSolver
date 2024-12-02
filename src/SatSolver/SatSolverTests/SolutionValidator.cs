1a7d59a8ba4a0fe9c52b2292e5c2f013c58e6175
René Vogt <rene.vogt@gmx.de>
2024-12-02 17:18:41 +0100

    Added solution validator to improve tests

---
+++ src/SatSolver/SatSolverTests/SolutionValidator.cs
@@ -0,0 +1,17 @@
+﻿using Revo.SatSolver;
+
+namespace SatSolverTests;
+
+static class SolutionValidator
+{
+    public static void Validate(Problem problem, Literal[] solution)
+    {
+        var variables = solution.ToDictionary(literal => literal.Id, literal => literal.Sense);
+        if (problem.Clauses.Any(clause => clause.Literals.Where(literal => variables.ContainsKey(literal.Id)).All(literal => variables[literal.Id] != literal.Sense)))
+            Assert.Fail("Solution does not work.");
+    }
+    public static void Validate(Problem problem, IEnumerable<Literal[]> solutions) 
+    {
+        foreach(var solution in solutions) Validate(problem, solution);
+    }
+}
