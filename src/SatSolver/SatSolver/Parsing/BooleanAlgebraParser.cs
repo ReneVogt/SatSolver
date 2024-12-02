f4ab5214785a08157f90aba432bb44e84c769497
René Vogt <rene.vogt@gmx.de>
2024-12-02 19:03:56 +0100

    sketched out boolean parser and its unit tests, still needs diagnostics

--- src/SatSolver/SatSolver/Parsing/BooleanAlgebraParser.cs
+++ src/SatSolver/SatSolver/Parsing/BooleanAlgebraParser.cs
@@ -4,8 +4,77 @@ namespace Revo.SatSolver.Parsing;
 
 public sealed class BooleanAlgebraParser
 {
-    BooleanAlgebraParser()
-    { }
+    readonly string _input;
 
-    public static BooleanExpression Parse(string input) => throw new NotImplementedException();
+    int _position;
+
+    bool EndReached => _position >= _input.Length;
+    char Current => !EndReached ? _input[_position] : '\0';
+
+    BooleanAlgebraParser(string input) => _input = input ?? throw new ArgumentNullException(nameof(input));
+
+    BooleanExpression Parse() => ParseExpression();
+
+    void SkipWhiteSpace()
+    {
+        while (!EndReached && char.IsWhiteSpace(Current)) _position++;
+    }
+
+    BooleanExpression ParseExpression(int parentPrecedence = 0)
+    {
+        SkipWhiteSpace();
+
+        BooleanExpression left;
+        if (Current == '!')
+        {
+            _position++;
+            left = new UnaryExpression(UnaryOperator.Not, ParseExpression(3));
+        }
+        else
+            left = ParsePrimaryExpression();
+        
+
+        for (; ; )
+        {
+            SkipWhiteSpace();
+            var current = Current;
+            var precedence = current switch
+            {
+                '&' => 2,
+                '|' => 1,
+                _ => 0
+            };
+            if (precedence <= parentPrecedence) return left;
+
+            _position++;
+            var right = ParseExpression(precedence);
+            left = new BinaryExpression(left, current == '&' ? BinaryOperator.And : BinaryOperator.Or, right);
+        }
+    }
+    BooleanExpression ParsePrimaryExpression()
+    {
+        SkipWhiteSpace();
+        return Current == '(' ? ParseParenthesizedExpression() : ParseLiteralExpression();
+    }
+    BooleanExpression ParseParenthesizedExpression()
+    {
+        _position++;
+        var expression = ParseExpression();
+        SkipWhiteSpace();
+        if (Current != ')') throw new NotImplementedException();
+        _position++;
+        return expression;
+    }
+    LiteralExpression ParseLiteralExpression()
+    {
+        var start = _position;
+        while (!EndReached && !(Current == '(' || Current == ')' || Current == '&' || Current == '|' || char.IsWhiteSpace(Current))) _position++;
+        return new LiteralExpression(_input[start.._position]);
+    }
+
+    public static BooleanExpression Parse(string input)
+    {
+        var parser = new BooleanAlgebraParser(input);
+        return parser.Parse();
+    }
 }
---
+++ src/SatSolver/SatSolverTests/Parsing/BooleanAlgebraParserTests.cs
@@ -0,0 +1,98 @@
+﻿using Revo.SatSolver.Parsing;
+using Revo.SatSolver.Parsing.Expressions;
+using Xunit.Abstractions;
+
+namespace SatSolverTests.Parsing;
+public class BooleanAlgebraParserTests(ITestOutputHelper? output)
+{
+    ITestOutputHelper? _output = output;
+
+    [Fact]
+    public void Parse_Literal()
+    {
+        const string input = "a";
+        var expression = BooleanAlgebraParser.Parse(input);
+        using var e = new BooleanExpressionAsserter(expression);
+
+        e.AssertLiteral("a");
+    }
+    [Fact]
+    public void Parse_Or()
+    {
+        const string input = "a | b";
+        var expression = BooleanAlgebraParser.Parse(input);
+        using var e = new BooleanExpressionAsserter(expression);
+        e.AssertOr();
+        e.AssertLiteral("a");
+        e.AssertLiteral("b");
+    }
+    [Fact]
+    public void Parse_And()
+    {
+        const string input = "a &\nb";
+        var expression = BooleanAlgebraParser.Parse(input);
+        using var e = new BooleanExpressionAsserter(expression);
+        e.AssertAnd();
+        e.AssertLiteral("a");
+        e.AssertLiteral("b");
+    }
+    [Fact]
+    public void Parse_Not()
+    {
+        const string input = "!not";
+        var expression = BooleanAlgebraParser.Parse(input);
+        using var e = new BooleanExpressionAsserter(expression);
+        e.AssertNot();
+        e.AssertLiteral("not");
+    }
+    [Fact]
+    public void Parse_ParenthesizedWithPrecedence()
+    {
+        const string input = "ab | (c & (a | d)) & !(x | !z) | !t";
+        var expression = BooleanAlgebraParser.Parse(input);
+        using var e = new BooleanExpressionAsserter(expression);
+        
+        e.AssertOr();
+            e.AssertOr();
+                e.AssertLiteral("ab");
+        
+                e.AssertAnd();
+
+                    e.AssertAnd();
+                        e.AssertLiteral("c");
+                        e.AssertOr();
+                            e.AssertLiteral("a");
+                            e.AssertLiteral("d");
+
+                    e.AssertNot();
+                        e.AssertOr();
+                            e.AssertLiteral("x");
+                            e.AssertNot();
+                                e.AssertLiteral("z");
+        
+            e.AssertNot();
+            e.AssertLiteral("t");
+    }
+
+    void DumpExpression(BooleanExpression expression, string indent = "")
+    {
+        switch (expression)
+        {
+            case BinaryExpression binaryExpression:
+                _output?.WriteLine($"{indent}{binaryExpression.Operator}");
+                _output?.WriteLine($"{indent} Left:");
+                DumpExpression(binaryExpression.Left, indent + "    ");
+                _output?.WriteLine($"{indent} Right:");
+                DumpExpression(binaryExpression.Right, indent + "    ");
+                break;
+            case UnaryExpression unaryExpression:
+                _output?.WriteLine($"{indent}Not");
+                _output?.WriteLine($"{indent} Expression:");
+                DumpExpression(unaryExpression.Expression, indent + "    ");
+                break;
+            case LiteralExpression literalExpression:
+                _output?.WriteLine($"{indent}'{literalExpression.Name}");
+                break;
+        }
+    }
+}
