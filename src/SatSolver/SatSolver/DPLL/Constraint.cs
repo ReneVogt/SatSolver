f869db91ed4755dec2a36fd00e20f4ce264fe6b8
René Vogt <rene.vogt@gmx.de>
2024-12-02 16:01:13 +0100

    completely refactored dpll algorithm. this is still very buggy..wip

---
+++ src/SatSolver/SatSolver/DPLL/Constraint.cs
@@ -0,0 +1,34 @@
+﻿namespace Revo.SatSolver.DPLL;
+
+sealed record Constraint(int Index)
+{
+    /// <summary>
+    /// The variables/literals in this constraint
+    /// that are not negated.
+    /// </summary>
+    public List<Variable> Positives { get; } = [];
+
+    /// <summary>
+    /// The negated variables/literals in 
+    /// this constraint.
+    /// </summary>
+    public List<Variable> Negatives { get; } = [];
+
+    /// <summary>
+    /// The number of not-negated literals still active 
+    /// in this constraint.
+    /// </summary>
+    public int NumberOfActivePositives { get; set; }
+
+    /// <summary>
+    /// The number of negated literals still active 
+    /// in this constraint.
+    /// </summary>
+    public int NumberOfActiveNegatives { get; set; }
+
+    /// <summary>
+    /// Indicates at which step this clause was removed to 
+    /// know when to put in use again during back tracking.
+    /// </summary>
+    public int RemovalIndex { get; set; } = -1;
+}
---
+++ src/SatSolver/SatSolver/DPLL/Variable.cs
@@ -0,0 +1,40 @@
+﻿namespace Revo.SatSolver.DPLL;
+
+sealed record Variable(int Index)
+{
+    /// <summary>
+    /// Indicates wether this variable is set
+    /// to <c>true</c> or <c>false</c>.
+    /// </summary>
+    public bool Sense { get; set; }
+
+    /// <summary>
+    /// Indicates if this is an already fixed
+    /// variable or if it is still free to test.
+    /// </summary>
+    public bool Fixed { get; set; }
+
+    /// <summary>
+    /// The clauses where this variable is contained
+    /// as not-negated literal.
+    /// </summary>
+    public List<Constraint> Positives { get; } = [];
+
+    /// <summary>
+    /// The clauses where this variable is contained
+    /// as negated literal.
+    /// </summary>
+    public List<Constraint> Negatives { get; } = [];
+
+    /// <summary>
+    /// The number of still active clauses containing
+    /// this variable as not-negated.
+    /// </summary>
+    public int NumberOfActivePositives { get; set; }
+
+    /// <summary>
+    /// The number of still active clauses containing
+    /// this variable as negated.
+    /// </summary>
+    public int NumberOfActiveNegatives { get; set; }
+}
