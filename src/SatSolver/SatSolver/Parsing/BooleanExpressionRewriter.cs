22227ac472cb1f617bb7a9f0a8f86d721b3c7462
René Vogt <rene.vogt@gmx.de>
2024-12-03 07:25:33 +0100

    introduce BooleanExpressionRewriter

---
+++ src/SatSolver/SatSolver/Parsing/BooleanExpressionRewriter.cs
@@ -0,0 +1,38 @@
+﻿using Revo.SatSolver.Parsing.Expressions;
+
+namespace Revo.SatSolver.Parsing;
+
+public abstract class BooleanExpressionRewriter
+{
+    protected BooleanExpressionRewriter()
+    {
+    }
+
+    public virtual BooleanExpression Rewrite(BooleanExpression expression) => expression switch
+    {
+        BinaryExpression binaryExpression => RewriteBinaryExpression(binaryExpression),
+        UnaryExpression unaryExpression => RewriteUnaryExpression(unaryExpression),
+        LiteralExpression literalExpression => RewriteLiteralExpression(literalExpression),
+        _ => expression
+    };
+
+    public virtual BooleanExpression RewriteBinaryExpression(BinaryExpression expression)
+    {
+        var left = Rewrite(expression.Left);
+        var right = Rewrite(expression.Right);
+
+        return left == expression.Left && right == expression.Right
+            ? expression
+            : new BinaryExpression(left, expression.Operator, right);
+    }
+
+    public virtual BooleanExpression RewriteUnaryExpression(UnaryExpression expression) 
+    {
+        var exp = Rewrite(expression.Expression);
+
+        return exp == expression.Expression
+            ? expression
+            : new UnaryExpression(expression.Operator, exp);
+    }
+    public virtual BooleanExpression RewriteLiteralExpression(LiteralExpression expression) => expression;
+}
