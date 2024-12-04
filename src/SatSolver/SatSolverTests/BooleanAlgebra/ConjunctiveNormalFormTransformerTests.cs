4d575e8962066be7be7c201ba893fb777735e2e6
René Vogt <rene.vogt@gmx.de>
2024-12-04 19:54:58 +0100

    added redundancy removal, but it's still buggy

--- src/SatSolver/SatSolverTests/BooleanAlgebra/ConjunctiveNormalFormTransformerTests.cs
+++ src/SatSolver/SatSolverTests/BooleanAlgebra/ConjunctiveNormalFormTransformerTests.cs
@@ -25,28 +25,43 @@ public class ConjunctiveNormalFormTransformerTests
     {
         const string input = "a | b";
         var expression = BooleanAlgebraParser.Parse(input);
-        Assert.Same(expression, Transform(expression));
+        Transform(expression)?.ToString().Should().Be(expression.ToString());
     }
     [Fact]
     public void Transform_SimpleAnd_Same()
     {
         const string input = "a & b";
         var expression = BooleanAlgebraParser.Parse(input);
-        Assert.Same(expression, Transform(expression));
+        Transform(expression)?.ToString().Should().Be(expression.ToString());
     }
     [Fact]
-    public void Transform_AndOfOr_Same()
+    public void Transform_AndOfOrLeft_Same()
+    {
+        const string input = "a & (b | c)";
+        var expression = BooleanAlgebraParser.Parse(input);
+        Transform(expression)?.ToString().Should().Be(expression.ToString());
+    }
+    [Fact]
+    public void Transform_AndOfOrRight_Same()
+    {
+        const string input = "(a | b) & c";
+        var expression = BooleanAlgebraParser.Parse(input);
+        Transform(expression)?.ToString().Should().Be(expression.ToString());
+    }
+    [Fact]
+    public void Transform_AndOfOrs_Same()
     {
         const string input = "(a | b) & (b | c)";
         var expression = BooleanAlgebraParser.Parse(input);
-        Assert.Same(expression, Transform(expression));
+        Transform(expression)?.ToString().Should().Be(expression.ToString());
     }
     [Fact]
-    public void Transform_OrOfAnd_Transformed()
+    public void Transform_OrOfAndLeft_Transformed()
     {
         const string input = "a | b & c";
         var expression = BooleanAlgebraParser.Parse(input);
         var transformed = Transform(expression);
+        Assert.NotNull(transformed);
         using var e = new BooleanExpressionAsserter(transformed);
 
         e.AssertAnd();
@@ -57,6 +72,25 @@ public class ConjunctiveNormalFormTransformerTests
         e.AssertLiteral("a");
         e.AssertLiteral("c");
     }
+    [Fact]
+    public void Transform_OrOfAndRight_Transformed()
+    {
+        const string input = "a & b | c";
+        var expression = BooleanAlgebraParser.Parse(input);
+        var transformed = Transform(expression);
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("((c | a) & (c | b))");
+
+        using var e = new BooleanExpressionAsserter(transformed);
+
+        e.AssertAnd();
+        e.AssertOr();
+        e.AssertLiteral("c");
+        e.AssertLiteral("a");
+        e.AssertOr();
+        e.AssertLiteral("c");
+        e.AssertLiteral("b");
+    }
 
     [Fact]
     public void Transform_OrOfAnds_Transformed()
@@ -65,6 +99,7 @@ public class ConjunctiveNormalFormTransformerTests
         var expression = BooleanAlgebraParser.Parse(input);
         expression.ToString().Should().Be("((a & b) | (c & d))");
         var transformed = Transform(expression);
+        Assert.NotNull(transformed);
         using var e = new BooleanExpressionAsserter(transformed);
         e.AssertAnd();
 
@@ -94,39 +129,72 @@ public class ConjunctiveNormalFormTransformerTests
         var expression = BooleanAlgebraParser.Parse(input);
         expression.ToString().Should().Be("((a & !(b | c)) | !(a & b))");
         var transformed = Transform(expression);
-        transformed.ToString().Should().Be("(((!a | !b) | a) & (((!a | !b) | !b) & ((!a | !b) | !c)))");
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("(!c | (!a | !b))");
+
+        using var e = new BooleanExpressionAsserter(transformed);
+        e.AssertOr();
+        e.AssertNot();
+        e.AssertLiteral("c");
+        e.AssertOr();
+        e.AssertNot();
+        e.AssertLiteral("a");
+        e.AssertNot();
+        e.AssertLiteral("b");
+    }
+
+    [Fact]
+    public void Transform_DistributedRedundancyRight_Removed()
+    {
+        var expression = BooleanAlgebraParser.Parse("a | (!a & b) | c");
+        expression.ToString().Should().Be("((a | (!a & b)) | c)");
+        var transformed = Transform(expression);
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("(a | (b | c))");
+
+        using var e = new BooleanExpressionAsserter(transformed);
+        e.AssertOr();
+        e.AssertLiteral("a");
+        e.AssertOr();
+        e.AssertLiteral("b");
+        e.AssertLiteral("c");
+    }
+    [Fact]
+    public void Transform_DistributedRedundancyLeft_Removed()
+    {
+        var expression = BooleanAlgebraParser.Parse("(!a & b) | c | a");
+        expression.ToString().Should().Be("(((!a & b) | c) | a)");
+        var transformed = Transform(expression);
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("(a | (c | b))");
+
+        using var e = new BooleanExpressionAsserter(transformed);
+        e.AssertOr();
+        e.AssertLiteral("a");
+        e.AssertOr();
+        e.AssertLiteral("c");
+        e.AssertLiteral("b");
+    }
+    [Fact]
+    public void Transform_XOR_Transformed()
+    {
+        var expression = BooleanAlgebraParser.Parse("a & !b | !a & b");
+        expression.ToString().Should().Be("((a & !b) | (!a & b))");
+        var transformed = Transform(expression);
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("((a | b) & (!b | !a))");
 
         using var e = new BooleanExpressionAsserter(transformed);
         e.AssertAnd();
-
         e.AssertOr();
-        e.AssertOr();
-        e.AssertNot();
         e.AssertLiteral("a");
-        e.AssertNot();
         e.AssertLiteral("b");
-        e.AssertLiteral("a");
-
-        e.AssertAnd();
         e.AssertOr();
-        e.AssertOr();
-        e.AssertNot();
-        e.AssertLiteral("a");
         e.AssertNot();
         e.AssertLiteral("b");
         e.AssertNot();
-        e.AssertLiteral("b");
-
-        e.AssertOr();
-        e.AssertOr();
-        e.AssertNot();
         e.AssertLiteral("a");
-        e.AssertNot();
-        e.AssertLiteral("b");
-        e.AssertNot();
-        e.AssertLiteral("c");
     }
-
 
     [Fact]
     public void Transform_2of3_Transformed()
@@ -134,7 +202,32 @@ public class ConjunctiveNormalFormTransformerTests
         const string input = "a & b & !c | a&!b&c | !a&b&c";
         var expression = BooleanAlgebraParser.Parse(input);
         var transformed = Transform(expression);
-        transformed.ToString().Should().Be("(((((((!a | (a | a)) & (!a | (a | !b))) & ((!a | (b | a)) & (!a | (b | !b)))) & (((b | (a | a)) & (b | (a | !b))) & ((b | (b | a)) & (b | (b | !b))))) & (((!a | (c | a)) & (!a | (c | b))) & ((b | (c | a)) & (b | (c | b))))) & ((((c | (a | a)) & (c | (a | !b))) & ((c | (b | a)) & (c | (b | !b)))) & ((c | (c | a)) & (c | (c | b))))) & (((((!a | (!c | a)) & (!a | (!c | !b))) & ((b | (!c | a)) & (b | (!c | !b)))) & (((!c | c) | !a) & ((!c | c) | b))) & (((c | (!c | a)) & (c | (!c | !b))) & (c | (!c | c)))))");
-        Assert.Fail("Boolean expressions should be reduced after converting to CNF. Remove redundand literals and trivially true clauses.");
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("");
     }
+
+    [Fact]
+    public void Transform_PartiallyAlwaysTrue()
+    {
+        const string input = "a & (b | c | !b) & (c | d)";
+        var expression = BooleanAlgebraParser.Parse(input);
+        var transformed = Transform(expression);
+        Assert.NotNull(transformed);
+        transformed.ToString().Should().Be("(a & (c | d))");
+        using var e = new BooleanExpressionAsserter(transformed);
+        e.AssertAnd();
+        e.AssertLiteral("a");
+        e.AssertOr();
+        e.AssertLiteral("c");
+        e.AssertLiteral("d");
+
+    }
+    [Fact]
+    public void Transform_AlwaysTrue_Null()
+    {
+        const string input = "((a | c) | !a) & (b | c | !b)";
+        var expression = BooleanAlgebraParser.Parse(input);
+        Transform(expression).Should().BeNull();
+    }
+
 }
