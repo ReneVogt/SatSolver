3da33840e5c870b6b0238abba8d2f5399f2d06fb
René Vogt <rene.vogt@gmx.de>
2024-12-04 17:18:17 +0100

    added string representation to expressions

--- src/SatSolver/SatSolver/BooleanAlgebra/ExpressionTreeWriter.cs
+++ src/SatSolver/SatSolver/BooleanAlgebra/ExpressionTreeWriter.cs
@@ -1,47 +1,62 @@
-﻿namespace Revo.SatSolver.BooleanAlgebra;
+﻿using System.Text;
+
+namespace Revo.SatSolver.BooleanAlgebra;
 
 public class ExpressionTreeWriter : BooleanExpressionRewriter
 {
-    readonly TextWriter _writer;
+    readonly TextWriter? _writer;
+    readonly StringBuilder? _builder;
 
-    protected ExpressionTreeWriter(TextWriter writer) =>
-        _writer = writer ?? throw new ArgumentNullException(nameof(writer));
+    protected ExpressionTreeWriter(TextWriter? writer = null, StringBuilder? builder = null) =>
+        (_writer, _builder) = (writer, builder);
 
     public override BooleanExpression RewriteBinaryExpression(BinaryExpression expression)
     {
         _ = expression ?? throw new ArgumentNullException(nameof(expression));
-        _writer.Write('(');
+        Write("(");
         Rewrite(expression.Left);
-        _writer.Write(expression.Operator == BinaryOperator.Or 
+        Write(expression.Operator == BinaryOperator.Or 
             ? " | " 
             : expression.Operator == BinaryOperator.And 
                 ? " & " 
                 : $" {expression.Operator} ");
         Rewrite(expression.Right);
-        _writer.Write(')');
+        Write(")");
         return expression;
     }
     public override BooleanExpression RewriteUnaryExpression(UnaryExpression expression)
     {
         _ = expression ?? throw new ArgumentNullException(nameof(expression));
-        _writer.Write(expression.Operator == UnaryOperator.Not ? "!" : expression.Operator.ToString());
+        Write(expression.Operator == UnaryOperator.Not ? "!" : expression.Operator.ToString());
         return base.RewriteUnaryExpression(expression);
     }
     public override BooleanExpression RewriteLiteralExpression(LiteralExpression expression)
     {
         _ = expression ?? throw new ArgumentNullException(nameof(expression));
-        _writer.Write(expression.Name);
+        Write(expression.Name);
         return base.RewriteLiteralExpression(expression);
     }
 
+    void Write(string text)
+    {
+        _writer?.Write(text);
+        _builder?.Append(text);
+    }
 
     /// <summary>
-    /// Writes the <paramref name="expression"/> to the <paramref name="writer"/> using indentation
-    /// steps of <paramref name="indentation"/> whitespaces.
+    /// Writes the <paramref name="expression"/> to the <paramref name="writer"/>.
     /// </summary>
     /// <param name="expression">The <see cref="BooleanExpression"/> to write out.</param>
     /// <param name="writer">The <see cref="TextWriter"/> to write to.</param>
-    /// <exception cref="ArgumentNullException"><paramref name="expression"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentNullException"><paramref name="expression"/> or <paramref name="writer"/> is <c>null</c>.</exception>
     public static void Write(BooleanExpression expression, TextWriter writer) =>
-        new ExpressionTreeWriter(writer).Rewrite(expression);
+        new ExpressionTreeWriter(writer: writer ?? throw new ArgumentNullException(nameof(writer))).Rewrite(expression);
+    /// <summary>
+    /// Writes the <paramref name="expression"/> to the <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="expression">The <see cref="BooleanExpression"/> to write out.</param>
+    /// <param name="builder">The <see cref="StringBuilder"/> to write to.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="expression"/> or <paramref name="builder"/> is <c>null</c>.</exception>
+    public static void Write(BooleanExpression expression, StringBuilder builder) =>
+        new ExpressionTreeWriter(builder: builder ?? throw new ArgumentNullException(nameof(builder))).Rewrite(expression);
 }
