f31dddffb8e81c7c62d3dc8226be58384a22e923
René Vogt <rene.vogt@gmx.de>
2024-12-04 10:59:35 +0100

    added string representation and hashcode

--- src/SatSolver/SatSolver/Literal.cs
+++ src/SatSolver/SatSolver/Literal.cs
@@ -7,6 +7,9 @@
 /// <param name="Sense">The sense (or value) of this literal.</param>
 public sealed record Literal(int Id, bool Sense)
 {
+    public override int GetHashCode() => (Sense ? Id : -Id).GetHashCode();
+    public override string ToString() => $"{(Sense ? Id : -Id)}";
+
     public static implicit operator Literal(int id) => new(Math.Abs(id), id > 0);
 }
 
