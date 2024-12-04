0a0b59b716aaae9d7e164b2cfdbcbf6ef9366991
René Vogt <rene.vogt@gmx.de>
2024-12-04 13:19:56 +0100

    added/refactored string representation, equality, comparison

--- src/SatSolver/SatSolver/Clause.cs
+++ src/SatSolver/SatSolver/Clause.cs
@@ -5,26 +5,59 @@ namespace Revo.SatSolver;
 /// <summary>
 /// Represents a clause in a SATisfiability <see cref="Problem"/>.
 /// </summary>
-/// <param name="Literals">An array of <see cref="Literal"/>s contained in this clause.</param>
-public sealed class Clause
+public sealed class Clause : IComparable<Clause>, IEquatable<Clause>
 {
+    /// <summary>
+    /// An array of <see cref="Literal"/>s contained in this clause.
+    /// The literals are sorted by their <see cref="Literal.Id"/> and 
+    /// then by their <see cref="Literal.Sense"/>.
+    /// </summary>
     public ImmutableArray<Literal> Literals { get; }
-    
+
+    /// <summary>
+    /// Creates a new <see cref="Clause"/> instance with the
+    /// given <paramref name="literals"/>.
+    /// </summary>
+    /// <param name="literals">A sequence of <see cref="Literal"/>s. These will
+    /// be sorted by <see cref="Literal.Id"/> and then <see cref="Literal.Sense"/>.</param>
+    /// <exception cref="ArgumentNullException"><paramref name="literals"/> is <c>null</c>.</exception>    
     public Clause(IEnumerable<Literal> literals)
     {
         _ = literals ?? throw new ArgumentNullException(nameof(literals));
         Literals = literals.OrderBy(literal => literal.Id).ThenBy(literal => literal.Sense).ToImmutableArray();
     }
 
-    public override int GetHashCode() =>  Literals.Aggregate(17, (hash, value) => hash * 371 + value.GetHashCode());
-    public override bool Equals(object? obj) => 
-        obj is Clause { Literals: var other } && 
-        other.Length == Literals.Length && 
-        other.SequenceEqual(Literals);
+    public override int GetHashCode() =>  Literals.Aggregate(Literals.Length.GetHashCode(), (hash, literal) => hash * 371 + literal.GetHashCode());
+    public override bool Equals(object? obj) => obj is Clause other && Equals(other); 
+    public bool Equals(Clause? other) => other?.Literals.SequenceEqual(Literals) ?? false;
+    public int CompareTo(Clause? other)
+    {
+        if (other is not { Literals: var literals}) return 1;
+        if (Literals.Length < literals.Length) return -1;
+        if (Literals.Length > literals.Length) return 1;
+        for (var i=0; i<Literals.Length; i++)
+        {
+            var mine = Literals[i];
+            var others = literals[i];
+            if (mine.Id < others.Id) return -1;
+            if (mine.Id > others.Id) return 1;
+            if (!mine.Sense && others.Sense) return -1;
+            if (mine.Sense && !others.Sense) return 1;
+        }
+
+        return 0;
+    }
 
     public override string ToString() => string.Join(" ", Literals) + " 0";
 
     public static implicit operator Clause(Literal[] literals) => new(literals);
     public static implicit operator Clause(int[] literals) => new(literals.Select(i => (Literal)i).ToArray());
+
+    public static bool operator ==(Clause left, Clause right) => left is null ? right is null : left.Equals(right);
+    public static bool operator !=(Clause left, Clause right) => left is null ? right is not null : !left.Equals(right);
+    public static bool operator <(Clause left, Clause right) => left is null ? right is not null : left.CompareTo(right) < 0;
+    public static bool operator <=(Clause left, Clause right) => left is null || left.CompareTo(right) <= 0;
+    public static bool operator >(Clause left, Clause right) => left is not null && left.CompareTo(right) > 0;
+    public static bool operator >=(Clause left, Clause right) => left is null ? right is null : left.CompareTo(right) >= 0;
 }
 
