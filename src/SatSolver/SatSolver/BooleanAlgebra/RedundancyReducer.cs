6772992fd0dcf3f245b9261940bdbac292895648
René Vogt <rene.vogt@gmx.de>
2024-12-05 15:44:44 +0100

    introduce RedundancyReducer

---
+++ src/SatSolver/SatSolver/BooleanAlgebra/RedundancyReducer.cs
@@ -0,0 +1,25 @@
+﻿namespace Revo.SatSolver.BooleanAlgebra;
+
+/// <summary>
+/// Reduces redundancies in <see cref="BooleanExpression"/>s.
+/// </summary>
+public class RedundancyReducer : BooleanExpressionRewriter
+{
+    RedundancyReducer()
+    {
+
+    }
+
+    /// <summary>
+    /// Removes redundancies from a boolean <paramref name="expression"/> expression.
+    /// Removes redundant literals (as in 'a | a' or 'a & a'), converts tautologies
+    /// (like 'a | !a') and contradictions (like 'a & !a') to the respective constant
+    /// expressions and removes supersets of literals in OR-expressions (like 
+    /// '(a | b) & (a | b | c) => (a | b)').
+    /// </summary>
+    /// <param name="expression">The <see cref="BooleanExpression"/> to reduce.</param>
+    /// <returns>A <see cref="BooleanExpression"/> representing the reduced expression tree.</returns>
+    /// <exception cref="ArgumentNullException">The <paramref name="expression"/> is <c>null</c>.</exception>
+    public static BooleanExpression Reduce(BooleanExpression expression) =>
+        new RedundancyReducer().Rewrite(expression);
+}
---
+++ src/SatSolver/SatSolverTests/BooleanAlgebra/RedundancyReducerTests.cs
@@ -0,0 +1,12 @@
+﻿using static Revo.SatSolver.BooleanAlgebra.RedundancyReducer;
+
+namespace SatSolverTests.BooleanAlgebra;
+
+public class RedundancyReducerTests
+{
+    [Fact]
+    public void Reduce_Null_ArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => Reduce(null!));
+    }
+}
