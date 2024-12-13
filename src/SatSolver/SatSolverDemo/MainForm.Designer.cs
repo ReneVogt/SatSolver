165ba88209daab1f253c1b7e84dfc9e29bd2285b
René Vogt <rene.vogt@gmx.de>
2024-12-13 18:20:49 +0100

    implemented ui

--- src/SatSolver/SatSolverDemo/MainForm.Designer.cs
+++ src/SatSolver/SatSolverDemo/MainForm.Designer.cs
@@ -40,15 +40,27 @@
             label3 = new Label();
             lvSolutions = new ListView();
             tmSolve = new System.Windows.Forms.Timer(components);
+            pbSolving = new ProgressBar();
+            outerContainer = new SplitContainer();
+            innerContainer = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)outerContainer).BeginInit();
+            outerContainer.Panel1.SuspendLayout();
+            outerContainer.Panel2.SuspendLayout();
+            outerContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)innerContainer).BeginInit();
+            innerContainer.Panel1.SuspendLayout();
+            innerContainer.Panel2.SuspendLayout();
+            innerContainer.SuspendLayout();
             SuspendLayout();
             // 
             // rbtBooleanAlgebra
             // 
             rbtBooleanAlgebra.AutoSize = true;
             rbtBooleanAlgebra.Checked = true;
-            rbtBooleanAlgebra.Location = new Point(12, 12);
+            rbtBooleanAlgebra.Location = new Point(11, 9);
+            rbtBooleanAlgebra.Margin = new Padding(2);
             rbtBooleanAlgebra.Name = "rbtBooleanAlgebra";
-            rbtBooleanAlgebra.Size = new Size(165, 29);
+            rbtBooleanAlgebra.Size = new Size(110, 19);
             rbtBooleanAlgebra.TabIndex = 1;
             rbtBooleanAlgebra.TabStop = true;
             rbtBooleanAlgebra.Text = "Boolean algebra";
@@ -60,9 +72,10 @@
             rbtDimacs.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             rbtDimacs.AutoSize = true;
             rbtDimacs.CheckAlign = ContentAlignment.MiddleRight;
-            rbtDimacs.Location = new Point(924, 12);
+            rbtDimacs.Location = new Point(601, 9);
+            rbtDimacs.Margin = new Padding(2);
             rbtDimacs.Name = "rbtDimacs";
-            rbtDimacs.Size = new Size(104, 29);
+            rbtDimacs.Size = new Size(69, 19);
             rbtDimacs.TabIndex = 2;
             rbtDimacs.Text = "DIMACS";
             rbtDimacs.UseVisualStyleBackColor = true;
@@ -70,32 +83,36 @@
             // 
             // rtbInput
             // 
-            rtbInput.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            rtbInput.Location = new Point(12, 47);
+            rtbInput.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            rtbInput.Location = new Point(11, 29);
+            rtbInput.Margin = new Padding(2);
             rtbInput.Name = "rtbInput";
-            rtbInput.Size = new Size(1016, 118);
+            rtbInput.ScrollBars = RichTextBoxScrollBars.Vertical;
+            rtbInput.Size = new Size(659, 86);
             rtbInput.TabIndex = 3;
             rtbInput.Text = "";
             rtbInput.TextChanged += OnInputChanged;
             // 
             // tbCnf
             // 
-            tbCnf.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            tbCnf.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             tbCnf.BackColor = SystemColors.Window;
-            tbCnf.Location = new Point(12, 206);
+            tbCnf.Location = new Point(11, 28);
+            tbCnf.Margin = new Padding(2);
             tbCnf.Multiline = true;
             tbCnf.Name = "tbCnf";
             tbCnf.ReadOnly = true;
             tbCnf.ScrollBars = ScrollBars.Vertical;
-            tbCnf.Size = new Size(1016, 93);
+            tbCnf.Size = new Size(659, 114);
             tbCnf.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(12, 178);
+            label1.Location = new Point(11, 8);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(213, 25);
+            label1.Size = new Size(144, 15);
             label1.TabIndex = 5;
             label1.Text = "Conjunctive normal form:";
             // 
@@ -103,72 +120,136 @@
             // 
             tbDimacs.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             tbDimacs.BackColor = SystemColors.Window;
-            tbDimacs.Location = new Point(12, 347);
+            tbDimacs.Location = new Point(11, 27);
+            tbDimacs.Margin = new Padding(2);
             tbDimacs.Multiline = true;
             tbDimacs.Name = "tbDimacs";
             tbDimacs.ReadOnly = true;
             tbDimacs.ScrollBars = ScrollBars.Both;
-            tbDimacs.Size = new Size(397, 279);
+            tbDimacs.Size = new Size(277, 232);
             tbDimacs.TabIndex = 6;
             tbDimacs.WordWrap = false;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 319);
+            label2.Location = new Point(11, 8);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(83, 25);
+            label2.Size = new Size(54, 15);
             label2.TabIndex = 7;
             label2.Text = "DIMACS:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(425, 319);
+            label3.Location = new Point(292, 8);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(90, 25);
+            label3.Size = new Size(59, 15);
             label3.TabIndex = 8;
             label3.Text = "Solutions:";
             // 
             // lvSolutions
             // 
+            lvSolutions.AllowColumnReorder = true;
             lvSolutions.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            lvSolutions.Location = new Point(425, 347);
+            lvSolutions.FullRowSelect = true;
+            lvSolutions.GridLines = true;
+            lvSolutions.Location = new Point(292, 27);
+            lvSolutions.Margin = new Padding(2);
             lvSolutions.Name = "lvSolutions";
-            lvSolutions.Size = new Size(603, 279);
+            lvSolutions.OwnerDraw = true;
+            lvSolutions.ShowGroups = false;
+            lvSolutions.Size = new Size(387, 232);
             lvSolutions.TabIndex = 9;
             lvSolutions.UseCompatibleStateImageBehavior = false;
             lvSolutions.View = View.Details;
+            lvSolutions.DrawColumnHeader += OnDrawSolutionColumn;
+            lvSolutions.DrawSubItem += OnDrawSolutionSubItem;
             // 
             // tmSolve
             // 
             tmSolve.Interval = 500;
             tmSolve.Tick += OnSolveTimer;
             // 
+            // pbSolving
+            // 
+            pbSolving.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pbSolving.Location = new Point(0, 264);
+            pbSolving.MarqueeAnimationSpeed = 10;
+            pbSolving.Name = "pbSolving";
+            pbSolving.Size = new Size(681, 13);
+            pbSolving.TabIndex = 10;
+            // 
+            // outerContainer
+            // 
+            outerContainer.Dock = DockStyle.Fill;
+            outerContainer.Location = new Point(0, 0);
+            outerContainer.Name = "outerContainer";
+            outerContainer.Orientation = Orientation.Horizontal;
+            // 
+            // outerContainer.Panel1
+            // 
+            outerContainer.Panel1.Controls.Add(innerContainer);
+            // 
+            // outerContainer.Panel2
+            // 
+            outerContainer.Panel2.Controls.Add(label2);
+            outerContainer.Panel2.Controls.Add(pbSolving);
+            outerContainer.Panel2.Controls.Add(tbDimacs);
+            outerContainer.Panel2.Controls.Add(lvSolutions);
+            outerContainer.Panel2.Controls.Add(label3);
+            outerContainer.Size = new Size(681, 568);
+            outerContainer.SplitterDistance = 284;
+            outerContainer.TabIndex = 11;
+            // 
+            // innerContainer
+            // 
+            innerContainer.Dock = DockStyle.Fill;
+            innerContainer.Location = new Point(0, 0);
+            innerContainer.Name = "innerContainer";
+            innerContainer.Orientation = Orientation.Horizontal;
+            // 
+            // innerContainer.Panel1
+            // 
+            innerContainer.Panel1.Controls.Add(rbtBooleanAlgebra);
+            innerContainer.Panel1.Controls.Add(rbtDimacs);
+            innerContainer.Panel1.Controls.Add(rtbInput);
+            // 
+            // innerContainer.Panel2
+            // 
+            innerContainer.Panel2.Controls.Add(label1);
+            innerContainer.Panel2.Controls.Add(tbCnf);
+            innerContainer.Size = new Size(681, 284);
+            innerContainer.SplitterDistance = 128;
+            innerContainer.TabIndex = 0;
+            // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1040, 660);
-            Controls.Add(lvSolutions);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(tbDimacs);
-            Controls.Add(label1);
-            Controls.Add(tbCnf);
-            Controls.Add(rtbInput);
-            Controls.Add(rbtDimacs);
-            Controls.Add(rbtBooleanAlgebra);
+            ClientSize = new Size(681, 568);
+            Controls.Add(outerContainer);
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(4, 5, 4, 5);
             MaximizeBox = false;
             MinimizeBox = false;
-            MinimumSize = new Size(700, 600);
+            MinimumSize = new Size(500, 400);
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "SatSolver Demo";
+            outerContainer.Panel1.ResumeLayout(false);
+            outerContainer.Panel2.ResumeLayout(false);
+            outerContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)outerContainer).EndInit();
+            outerContainer.ResumeLayout(false);
+            innerContainer.Panel1.ResumeLayout(false);
+            innerContainer.Panel1.PerformLayout();
+            innerContainer.Panel2.ResumeLayout(false);
+            innerContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)innerContainer).EndInit();
+            innerContainer.ResumeLayout(false);
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -182,5 +263,8 @@
         private Label label3;
         private ListView lvSolutions;
         private System.Windows.Forms.Timer tmSolve;
+        private ProgressBar pbSolving;
+        private SplitContainer outerContainer;
+        private SplitContainer innerContainer;
     }
 }
