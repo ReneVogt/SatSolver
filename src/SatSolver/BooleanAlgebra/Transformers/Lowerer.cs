58eb991b9195ae301a13c488bc81372f76e6fc8a
René Vogt <rene.vogt@gmx.de>
2024-12-15 15:16:14 +0100

    added lowerer that removes complex operators (currently namely xor %) from expressions so that only AND, OR and NOT remain

---
+++ src/SatSolver/BooleanAlgebra/Transformers/Lowerer.cs
@@ -0,0 +1,102 @@
+﻿using Revo.BooleanAlgebra.Expressions;
+using static Revo.BooleanAlgebra.Expressions.BooleanExpressionException;
+using static Revo.BooleanAlgebra.Expressions.ExpressionFactory;
+
+namespace Revo.BooleanAlgebra.Transformers;
+
+/// <summary>
+/// This transformer takes a <see cref="BooleanExpression"/> and removes
+/// all operations "higher" than AND, OR and NOT.
+/// So for the moment, a % b will be lowererd to (a | b) & (!a | !b).
+/// Double negations will be removed.
+/// Constant expressions will be propagated.
+/// </summary>
+public class Lowerer : BooleanExpressionRewriter
+{
+    protected Lowerer()
+    {
+    }
+
+    public override BooleanExpression RewriteUnaryExpression(UnaryExpression expression)
+    {
+        _ = expression ?? throw new ArgumentNullException(nameof(expression));
+        if (expression.Operator != UnaryOperator.Not) throw UnsupportedUnaryOperator(expression.Operator);
+
+        //
+        // If the inner expression is a constant, we
+        // return the negated constant.
+        //
+        if (expression.Expression.Kind == ExpressionKind.Constant && expression.Expression is ConstantExpression { Sense: var sense })
+            return sense ? Zero : One;
+
+        //
+        // If this is a double negation it can be skipped
+        // and the inner epression should be rewritten and
+        // returned.
+        //
+        if (expression.Expression.Kind == ExpressionKind.Unary && expression.Expression is UnaryExpression { Operator: var op, Expression: var doubleNegatedExpression })
+        {
+            if (op !=  UnaryOperator.Not) throw UnsupportedUnaryOperator(op);
+            return Rewrite(doubleNegatedExpression);
+        }
+
+        return base.RewriteUnaryExpression(expression);
+    }
+
+    public override BooleanExpression RewriteBinaryExpression(BinaryExpression expression)
+    {
+        _ = expression ?? throw new ArgumentNullException(nameof(expression));
+
+        //
+        // XOR expressions like a % b are rewritten as (a | b) & (!a | !b) and 
+        // this converted expression is rewritten again to transform children.
+        //
+        if (expression.Operator == BinaryOperator.Xor)
+            return Rewrite(expression.Left.Or(expression.Right).And(Not(expression.Left).Or(Not(expression.Right))));
+
+        if (expression.Operator != BinaryOperator.Or && expression.Operator != BinaryOperator.And) throw UnsupportedBinaryOperator(expression.Operator);
+
+        var left = Rewrite(expression.Left);
+        if (left.Kind == ExpressionKind.Constant && left is ConstantExpression { Sense: var leftSense })
+        {
+            if (expression.Operator == BinaryOperator.And)
+            {
+                if (!leftSense) return Zero;
+                return Rewrite(expression.Right);
+            }
+            if (expression.Operator == BinaryOperator.Or)
+            {
+                if (leftSense) return One;
+                return Rewrite(expression.Right);
+            }
+        }
+
+        var right = Rewrite(expression.Right);
+        if (right.Kind == ExpressionKind.Constant && right is ConstantExpression { Sense: var rightSense })
+        {
+            if (expression.Operator == BinaryOperator.And)
+            {
+                if (!rightSense) return Zero;
+                return left;
+            }
+            if (expression.Operator == BinaryOperator.Or)
+            {
+                if (rightSense) return One;
+                return Rewrite(left);
+            }
+        }
+
+        return left == expression.Left && right == expression.Right 
+            ? expression
+            : expression.Operator == BinaryOperator.And ? left.And(right) : left.Or(right);
+    }
+
+    /// <summary>
+    /// Transforms the given <see cref="BooleanExpression"/> to use only OR, AND and NOT operations.
+    /// It also propagates constants and removes double negations.
+    /// </summary>
+    /// <param name="expression">The <see cref="BooleanExpression"/> to transform.</param>
+    /// <returns>The lowered <see cref="BooleanExpression"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="expression"/> is <c>null</c>.</exception>
+    public static BooleanExpression Lower(BooleanExpression expression) => new Lowerer().Rewrite(expression);
+}
---
+++ src/SatSolver/BooleanAlgebraTests/Rewriting/LowererTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Revo.BooleanAlgebra.Parsing;
+using static Revo.BooleanAlgebra.Transformers.Lowerer;
+
+namespace BooleanAlgebraTests.Rewriting;
+
+public class LowererTests
+{
+    [Fact]
+    public void Lower_Null_ArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => Lower(null!));
+    }
+
+    [
+        Theory,
+        InlineData("test"),
+        InlineData("a | b"),
+        InlineData("a & b"),
+        InlineData("a & (b | c)"),
+        InlineData("(a | b) & c"),
+        InlineData("(a | b) & (b | c)")
+    ]
+    public void Lower_IdentityForSimpleCases(string input)
+    {
+        var expression = BooleanAlgebraParser.Parse(input);
+        Assert.Same(expression, Lower(expression));
+    }
+
+    [
+        Theory,
+        InlineData("a % b", "(a | b) & (!a | !b)"),
+        InlineData("a & b | (c % !!d)", "a & b | (c | d) & (!c | !d)"),
+        InlineData("a & b | c & 1", "a & b | c"),
+        InlineData("a & b | c & 0", "a & b")
+    ]
+    public void Lower_CorrectTransformation(string input, string expected)
+    {
+        var expression = BooleanAlgebraParser.Parse(input);
+        Lower(expression).ToString().Should().Be(expected);
+    }
+}
