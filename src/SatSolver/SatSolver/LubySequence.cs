d6dda06e178518cf16b53beeeb44f08bb27aae47
René Vogt <rene.vogt@gmx.de>
2025-05-10 16:07:52 +0200

    implemented Luby sequence

---
+++ src/SatSolver/SatSolver/LubySequence.cs
@@ -0,0 +1,28 @@
+﻿namespace Revo.SatSolver;
+
+sealed class LubySequence(long baseValue = 1)
+{
+    long _u = 1, _v = 1;
+
+    public long Next()
+    {
+        //
+        // Donald Knuth's 'reluctant doubling' formula
+        //
+        var next = _v;
+        if ((_u & (-_u)) == _v)
+        {
+            _u += 1;
+            _v = 1;
+        } 
+        else _v *= 2;
+
+        return next * baseValue;
+    }
+
+    public static IEnumerable<long> Enumerate(long baseValue = 1)
+    {
+        var sequence = new LubySequence(baseValue);
+        for (; ; ) yield return sequence.Next();
+    }
+}
