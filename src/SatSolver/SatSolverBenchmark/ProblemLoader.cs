2dc107de6db10807725bce6748b7aed8ad7cde60
René Vogt <rene.vogt@gmx.de>
2025-05-17 16:45:39 +0200

    added BenchmarkDotNet

---
+++ src/SatSolver/SatSolverBenchmark/ProblemLoader.cs
@@ -0,0 +1,16 @@
+﻿using Revo.SatSolver;
+using Revo.SatSolver.Parsing;
+
+namespace SatSolverBenchmark;
+static class ProblemLoader
+{
+    public static ((string Name, Problem Problem)[] Sat, (string Name, Problem Problem)[] Unsat) LoadProblems()
+    {
+        var satProblems = Directory.GetFiles("SAT", "*.cnf")
+            .Select(file => (file, problem: DimacsParser.Parse(File.ReadAllText(file)).Single())).OrderBy(x => x.file).ToArray();
+        var unsatProblems = Directory.GetFiles("UNSAT", "*.cnf")
+            .Select(file => (file, problem: DimacsParser.Parse(File.ReadAllText(file)).Single())).OrderBy(x => x.file).ToArray();
+
+        return (satProblems, unsatProblems);
+    }
+}
---
+++ src/SatSolver/SatSolverBenchmark/TestOptions.cs
@@ -0,0 +1,46 @@
+﻿using Revo.SatSolver;
+
+namespace SatSolverBenchmark;
+static class TestOptions
+{
+    public static SatSolver.Options Default { get; } = new SatSolver.Options()
+    {
+        OnlyPoorMansVSIDS = true,
+
+        VariableActivityDecayFactor = 0.9995,
+
+        ClauseActivityDecayFactor = 0.999,
+        LiteralBlockDistanceMaximum = 8,
+        MaximumClauseMinimizationDepth = 9,
+
+        ClauseDeletion = new()
+        {
+            LiteralBlockDistanceToKeep = 2,
+            OriginalClauseCountFactor = 5,
+            RatioToDelete = 0.5,
+            LiteralBlockDistanceThreshold = 1.3,
+            PropagationRateThreshold = 0.5
+        },
+
+        Restart = new()
+        {
+            Interval = null,
+            Luby = false,
+            LiteralBlockDistanceThreshold = null,
+            PropagationRateThreshold = null
+        },
+
+        LiteralBlockDistanceTracking = new()
+        {
+            Decay = 0.999,
+            RecentCount = 100
+        },
+
+        PropagationRateTracking = new()
+        {
+            ConflictInterval = 500,
+            Decay = 0.999,
+            SampleSize = 50
+        }
+    };
+}
